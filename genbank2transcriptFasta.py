3c121055c235fc91c514410b3cbda8a4184dcce7
cjbparlant <cjbparlant@ls28.crchul.ulaval.ca>
2012-03-16 12:31:38 -0400

    Changed for phage gbk

--- genbank2transcriptFasta.py
+++ genbank2transcriptFasta.py
@@ -1,23 +1,56 @@
 #!/usr/bin/python
+# encoding: utf-8
 
+"""
+usage:
+
+DataStoreMerger.py <fileName>
+fileName: File in genbanks format
+
+"""
+
+import sys
 import Bio
 from Bio import SeqIO
 
-input_handle = open("S_thermo_DGCC7710.gbk", "rU")
+def getFeatureQualifier(gb_features, qualifier):
+	answer=""
+	for value in gb_features.qualifiers:
+		if value == qualifier:
+			answer =  gb_features.qualifiers[value]
+	
+	if answer != "":
+		return answer
+	else:
+		print 'Incorrect qualifier: ' + qualifier
+		sys.exit(1)
 
-count = 0
-genome = SeqIO.read(input_handle, "genbank")
+if __name__=="__main__":
+	if len(sys.argv)!=2:
+		print __doc__
+		sys.exit(1)
 
-for gb_features in genome.features:
-	f_type = gb_features.type
-	if (f_type != 'gene' and f_type != 'Source' 
-	    and f_type != 'fasta_record'):
-		header = str(gb_features.qualifiers)
-		seqStart = gb_features.location.start
-		seqEnd = gb_features.location.end
-		print "> " + gb_features.type + " " + header
-		print genome.seq[seqStart:seqEnd]
+	fileName=sys.argv[1]
+	input_handle = open(fileName, "rU")
 
-output_handle.close()
+	count = 0
+	genome = SeqIO.read(input_handle, "genbank")
 
-print "Converted %i records" % count
+	for gb_features in genome.features:
+		f_type = gb_features.type
+		if (f_type != 'gene' and f_type != 'source' 
+		    and f_type != 'Source' and f_type != 'fasta_record'):
+			header = str(gb_features.qualifiers)
+			seqStart = gb_features.location.start
+			seqEnd = gb_features.location.end
+			print ">",
+			print getFeatureQualifier(gb_features, "protein_id"),
+			print "|product: ",
+			print getFeatureQualifier(gb_features, "product"),
+			print " (",
+			print getFeatureQualifier(gb_features, "db_xref"),
+			print  ")"
+			#print ">" + gb_features.protein_id + "|product:" + gb_features.product + " (" + gb_features.db_xref + ")" 
+			print genome.seq[seqStart:seqEnd]
+
+	input_handle.close()
